4f8dd7c13609cbc465d460c2730e90f551c00edf
arthurzop <medeirosartur48@gmail.com>
2024-01-26 20:15:58 -0300

    Logo do header pronta

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Logo from "../components/logo";
 
 export default function Home ({}) {
     return(
         <>
         <View>
+            <Logo/>
             <Text>
                 Home Page
             </Text>
